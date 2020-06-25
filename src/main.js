6faa940268ddc3cb1ead14e9f7666f68d5765384
Malejandraib <mariaibanez07@gmail.com>
2020-06-25 12:16:44 -0500

    template string halar data

--- src/main.js
+++ src/main.js
@@ -1,51 +1,30 @@
 //import { objectToArray } from './data.js';
-import {anotherExample} from './data.js';
+//import {anotherExample} from './data.js';
 import data from './data/lol/lol.js';
 var lolData = Object.entries(data.data)
-console.log(lolData)
 
 let championsCards = document.querySelector('.card-champions');
 
-let items =Object.entries(data.data);
 
-var attribute = lolData.map(function(champions){
+window.onload = () =>{
+const attribute = lolData.map(champions=> `
   
-  const cards = document.createElement('article');
-  
-  const image = document.createElement('img');
-  image.setAttribute('src',champions[1].splash);
-  cards.appendChild(image)
+    <article class="card"> 
+      <img src = '${champions[1].splash}'>
+      <h2>${champions[1].name}</h2>
+      <p>${champions[1].title}</p>
+    </article>`
+    
+  ).join('');
 
-  const name = document.createElement('h2');
-  name.textContent = champions[1].name;
-  cards.appendChild(name); 
-
-  const title = document.createElement('h3');
-  title.textContent = champions[1].title;
-  cards.appendChild(title); 
-  
-   
-  
-  return championsCards.appendChild(cards);
-
-});
-
-
-console.log(attribute)
-
-
+ championsCards.innerHTML= attribute;
+};
 
 
  
-/* fetch(./data/lol/lol.json)
-.then((resp) => resp.json()) // Transform the data into json
-.then(function(data) {
-  // Create and append the li's to the ul
-  const table = createElement('span');
-    
-    table.appendChild(data.data.Alistar.tags);
-  })
-}); */
+
+
+
 
 
 
