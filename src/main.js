7f22ad3af8ea8c75e4ef32c5bab17b04ed78b4b8
AnaRativa09 <anarativa09@gmail.com>
2020-06-28 17:25:57 -0500

    Borrar otras datas

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-//import { objectToArray } from './data.js';
-//import {anotherExample} from './data.js';
 import data from './data/lol/lol.js';
 const lolData = Object.entries(data.data)
 
 let championsCards = document.querySelector('.card-champions');
-
 
 window.onload = () =>{
 const attribute = lolData.map(champions=> `
   
     <article style= "background-image:url('${champions[1].splash}')"  class="card"> 
       <div class="name-title-container">
-       <h2>${champions[1].name}</h2>
-       <p>${champions[1].title}</p>
+        <h2>${champions[1].name}</h2>
+        <p>${champions[1].title}</p>
       </div>
     </article>`
     
