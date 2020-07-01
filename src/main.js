1dbc3e153b7b7c9e22cfd51d1e79bbf21d1037db
Alefyyyy <Alejndraprz@gmail.com>
2020-07-01 16:54:37 -0500

    :D

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-const viewMain = document.getElementById("viewMain"); 
-const viewChampions = document.getElementById("viewChampions"); 
 
-viewChampions.style.display = "none";
 
 console.log(example, data);
