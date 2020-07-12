c58763d67c93658ae3c6580442aaeeb41ccabb46
Gabriela <66085174+gabriela-rincon@users.noreply.github.com>
2020-07-12 10:28:17 -0500

    Muestra da la data lol en el console

--- src/data.js
+++ src/data.js
@@ -1,9 +1,22 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+//  return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
+import champions from './data/lol/lol.js';
+
+let dataChampions = Object.values(champions.data);
+
+console.log(dataChampions);
+
+export let list = () => {
+  return dataChampions;
+}
+
+export default {
+  list
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data.js';
 
-console.log(example, data);
+const dataChampions = data.list();
+
+//const dataChampions.array.forEach(element => {
+//  const chamContainer = document.getElementById('chamContainer')
+//});
