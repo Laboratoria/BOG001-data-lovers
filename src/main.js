3cb1791907b0f2f402bb53ceb5532ebca7fde212
ApuntaDePuntos <apuntadepuntos@gmail.com>
2020-07-15 09:09:16 -0500

    check

--- src/main.js
+++ src/main.js
@@ -7,19 +7,37 @@ const datos = data.results;
 
 let personajes = datos.slice(0, 99);
 
-  let names = [];
+function popUp(e) {
+
+  const personaje = e.target.dataset.personajes;
+
+  console.log(personaje)
+
+  let information = [];
+
+
+  information += `  <img src="${personajes[personaje].image} "><p> Name: ${personajes[personaje].name}
+      </p> <p> Gender: ${personajes[personaje].gender} </p> <p> Species: ${personajes[personaje].species} </p>
+      <p> Origin: ${personajes[personaje].origin.name} </p> `
+
+
+  var characterCards = document.getElementById("pruebaTarjeta");
+  characterCards.innerHTML = information;
+
+};
+
+
+
+let names = [];
 for (var i = 0; i < personajes.length; i++) {
-    names += `<button > <img src="${personajes[i].image} ">
-  <p> Nombre: ${personajes[i].name} </p> <id= ${personajes[i].id} >`
+  names += `<button id="${personajes[i].id}-${i}"> <img data-personajes="${i}" src="${personajes[i].image} ">
+  <p data-personajes="${i}" > Nombre: ${personajes[i].name} </p>  </button>`
 };
 var characterButtons = document.getElementById("charactersZone");
 characterButtons.innerHTML = names;
 
+for (var i = 0; i < personajes.length; i++) {
+  const boton = document.getElementById(`${personajes[i].id}-${i}`);
+  boton.addEventListener("click", popUp);
 
-//${onclick = information }
-
-
-
-
-
-
+}
