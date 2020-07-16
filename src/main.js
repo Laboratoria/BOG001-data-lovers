0e4a4039ea1f9bd1bd1ec3245652e6b79ff4bf9e
ApuntaDePuntos <apuntadepuntos@gmail.com>
2020-07-16 12:44:17 -0500

    Datos check

--- src/main.js
+++ src/main.js
@@ -4,40 +4,95 @@
 import data from './data/rickandmorty/rickandmorty.js';
 const datos = data.results;
 
-
 let personajes = datos.slice(0, 99);
+
+// PAGINA DE PERSONAJES
+console.log(window.location.pathname)
+if  (window.location.pathname === "/src/Personajes.html") { 
 
 function popUp(e) {
 
-  const personaje = e.target.dataset.personajes;
+  const indicator = e.target.dataset.personajes;
 
-  console.log(personaje)
+  
 
   let information = [];
 
 
-  information += `  <img src="${personajes[personaje].image} "><p> Name: ${personajes[personaje].name}
-      </p> <p> Gender: ${personajes[personaje].gender} </p> <p> Species: ${personajes[personaje].species} </p>
-      <p> Origin: ${personajes[personaje].origin.name} </p> `
+  information += `  <img src="${personajes[indicator].image} "><p> Name: ${personajes[indicator].name}
+      </p> <p> Gender: ${personajes[indicator].gender} </p> <p> Species: ${personajes[indicator].species} </p>
+      <p> Origin: ${personajes[indicator].origin.name} </p> `
 
 
-  var characterCards = document.getElementById("pruebaTarjeta");
+  let characterCards = document.getElementById("pruebaTarjeta");
   characterCards.innerHTML = information;
 
 };
 
 
-
 let names = [];
-for (var i = 0; i < personajes.length; i++) {
-  names += `<button id="${personajes[i].id}-${i}"> <img data-personajes="${i}" src="${personajes[i].image} ">
-  <p data-personajes="${i}" > Nombre: ${personajes[i].name} </p>  </button>`
+for (let i = 0; i < personajes.length; i++) {
+  names += `<button class= "personajes1"  id="${personajes[i].id}-${i}"> <img data-personajes="${i}" src="${personajes[i].image} ">
+  <p data-personajes="${i}" > Name: ${personajes[i].name} </p>  </button>`
 };
-var characterButtons = document.getElementById("charactersZone");
+
+let  characterButtons = document.getElementById("charactersZone");
+
+
+
 characterButtons.innerHTML = names;
 
-for (var i = 0; i < personajes.length; i++) {
+for (let i = 0; i < personajes.length; i++) {
   const boton = document.getElementById(`${personajes[i].id}-${i}`);
   boton.addEventListener("click", popUp);
 
-}
+};
+// esta } es cierre de if de la ventana
+};
+
+//PARA PAGINA MUNDOS 
+
+if  (window.location.pathname === "/src/Mundo.html") {
+
+// Para conseguir el nombre de los mundos 
+
+
+let world = [];
+for (let  i = 0; i < personajes.length; i++) {
+// saber si en world ya existe ese nombre del planeta . poner en el HTML 
+let  nombreplaneta = `<button> <p> World name: ${personajes[i].origin.name} </p>`
+
+ if ( !world.includes(nombreplaneta)) {world.push(nombreplaneta)} 
+ 
+
+};
+ 
+ console.log(world)
+
+ let  worldButtons = document.getElementById("worldZone");
+worldButtons.innerHTML = world;
+// esta } es cierre de if de la ventana
+}; 
+
+//PARA PAGINA DE TEMPORADAS
+
+if  (window.location.pathname === "/src/Temporada.html") {
+
+ // Para conseguir los Capitulos 
+
+let chapters = [];
+
+for (let  i = 0; i < personajes.length; i++) {
+// saber si en world ya existe ese nombre del capitulos 
+let nombrecapitulos = `<button> <p> World name: ${personajes[i].episode}</p>`
+
+ if ( !chapters.includes(nombrecapitulos)) {chapters.push(nombrecapitulos)} 
+
+};
+
+let  seasonButtons = document.getElementById("seasonZone");
+seasonButtons.innerHTML = chapters;
+ 
+ console.log(chapters)
+ // esta } es cierre de if de la ventana
+}; 
