72ee4d24b1a02c509da4d7b52908ba9e673d9592
dalejac <k-da929@hotmail.com>
2020-07-27 09:27:07 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 // import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+var next = document.getElementById("continue");
+
+next.addEventListener("click", nextPage);
+
+function nextPage() {
+    document.getElementById('window-1').style.display = 'none';
+    document.getElementById('window-2').style.display = 'block';
+} 
 
 console.log(data);
-
