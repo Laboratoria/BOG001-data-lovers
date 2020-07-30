f60980c161f5827b7fda6ef359b4f132f1c64fcc
ApuntaDePuntos <apuntadepuntos@gmail.com>
2020-07-30 11:08:52 -0500

    Jueves 30 Modal

--- src/main.js
+++ src/main.js
@@ -10,29 +10,37 @@ let personajes = datos.slice(0, 99);
 console.log(window.location.pathname)
 if (window.location.pathname === "/src/Personajes.html") {
 
-  function popUp(e , arrayPersonajes) {
+  function popUp(e, arrayPersonajes) {
     const indicator = e.target.dataset.personajes;
 
     let information = [];
     let characterCards = document.getElementById("pruebaTarjeta");
 
-    information += ` <div class= "tarjeta"> <img class="tarjetaImg" src="${arrayPersonajes[indicator].image} "> <div class="tarjetaText"> <p> Name: ${arrayPersonajes[indicator].name}
+
+    information += ` <div class= "tarjeta"> 
+      <img class="tarjetaImg" src="${arrayPersonajes[indicator].image} "> 
+      <div class="tarjetaText"> <p> Name: ${arrayPersonajes[indicator].name}
       </p> <p> Gender: ${arrayPersonajes[indicator].gender} </p> <p> Species: ${arrayPersonajes[indicator].species} </p>
-      <p> Origin: ${arrayPersonajes[indicator].origin.name} </p> <p> Status: ${arrayPersonajes[indicator].status} </p> </div> </div>`
+      <p> Origin: ${arrayPersonajes[indicator].origin.name} </p> <p> Status: ${arrayPersonajes[indicator].status} </p> </div> 
+      <div id="cerrarModal" class="modal_cerrar" > x <div>
+      
+        </div>`
 
     characterCards.innerHTML = information;
+    document.getElementById("pruebaTarjeta").style.visibility = "visible";
+    document.getElementById("cerrarModal").addEventListener("click", function () { 
+      document.getElementById("pruebaTarjeta").style.visibility = "hidden"; })
+    
   };
 
   function show(arrayPersonajes) {
 
     let names = [];
     for (let i = 0; i < arrayPersonajes.length; i++) {
-      names += `<div class= "personajes1"  id="${arrayPersonajes[i].id}-${i}">
+      names += `<div class= "personajes1" data-personajes="${i}" id="${arrayPersonajes[i].id}-${i}">
         <img class= "image" data-personajes="${i}" src="${arrayPersonajes[i].image} ">
       <p data-personajes="${i}" class= "nombres" >${arrayPersonajes[i].name.slice(0, 17)} </p>  </div>`
     };
-
-
 
     let characterButtons = document.getElementById("charactersZone");
 
@@ -41,7 +49,7 @@ if (window.location.pathname === "/src/Personajes.html") {
 
     for (let i = 0; i < arrayPersonajes.length; i++) {
       const boton = document.getElementById(`${arrayPersonajes[i].id}-${i}`);
-      boton.addEventListener("click", (e) => popUp (e , personajes) );
+      boton.addEventListener("click", (e) => popUp(e, personajes));
     };
 
     //Cierre funcion show
@@ -54,55 +62,56 @@ if (window.location.pathname === "/src/Personajes.html") {
 
     let alivePersonajesFiltros = arrayPersonajes.filter(personaje => personaje.status == "Alive");
     show(alivePersonajesFiltros);
-    
+
   }
   const aliveButton = document.getElementById("alive")
-  aliveButton.addEventListener("click", ()=> alive(personajes));
+  aliveButton.addEventListener("click", () => alive(personajes));
 
   // Metodo SORT();
 
-  function comparar(a, b)
-    {
-      if (a.name > b.name) {
-        return 1;
-      }
-      if (a.name < b.name) {
-        return -1;}
-       else { return 0 }
+  function comparar(a, b) {
+    if (a.name > b.name) {
+      return 1;
     }
-
-    function az(arrayPersonajes) {
-      let personajesOrdenados = arrayPersonajes.sort(comparar);
-      show(personajesOrdenados);
-      popUp (e , personajesOrdenados )
+    if (a.name < b.name) {
+      return -1;
     }
-    const sortButton = document.getElementById("sortButton")
-    sortButton.addEventListener("click", (e)=>az(personajes));
+    else { return 0 }
+  }
 
-    // Busqueda de muñecos 
+  function az(arrayPersonajes) {
+    let personajesOrdenados = arrayPersonajes.sort(comparar);
+    show(personajesOrdenados);
+    popUp(e, personajesOrdenados)
+  }
+  const sortButton = document.getElementById("sortButton")
+  sortButton.addEventListener("click", (e) => az(personajes));
 
-      function searching( string , arrayPersonajes ) {
-        let busqueda = arrayPersonajes.find( personaje => personaje.name.toLowerCase() === string.value.toLowerCase() );
-        show(busqueda);
-      };
-      
-        let  palabra = document.getElementById("searchButton");
-        
-        palabra.addEventListener("click", () => searching( palabra, personajes ));
+  // Busqueda de muñecos 
 
-    // esta } es cierre de if de la ventana
+  function searching(string, arrayPersonajes) {
+    let busqueda = arrayPersonajes.find(personaje => personaje.name.toLowerCase() === string.value.toLowerCase());
+    show(busqueda);
   };
 
-  //PARA PAGINA MUNDOS 
+  let palabra = document.getElementById("searchButton");
 
-  if (window.location.pathname === "/src/Mundo.html") {
+  palabra.addEventListener("click", () => searching(palabra, personajes));
 
-    // Para conseguir el nombre de los mundos 
+  // esta } es cierre de if de la ventana
+};
 
+//PARA PAGINA MUNDOS 
+
+if (window.location.pathname === "/src/Mundo.html") {
+
+  // Para conseguir el nombre de los mundos 
+
+  function newWorlds(arrayPersonajes) {
     let world = [];
-    for (let i = 0; i < personajes.length; i++) {
+    for (let i = 0; i < arrayPersonajes.length; i++) {
       // saber si en world ya existe ese nombre del planeta . poner en el HTML 
-      let nombreplaneta = `<div class= "personajesW" <img src="mundos.jpg"> <p> ${personajes[i].origin.name} </p> </div>`
+      let nombreplaneta = `<div class= "personajesW" <img src="mundos.jpg"> <p> ${arrayPersonajes[i].origin.name} </p> </div>`
 
       if (!world.includes(nombreplaneta)) { world.push(nombreplaneta) }
     };
@@ -110,46 +119,74 @@ if (window.location.pathname === "/src/Personajes.html") {
 
     let worldButtons = document.getElementById("worldZone");
     worldButtons.innerHTML = world;
+  }
 
-    // esta } es cierre de if de la ventana
+  newWorlds(personajes)
+  // esta } es cierre de if de la ventana
+};
+
+//PARA PAGINA DE TEMPORADAS
+
+if (window.location.pathname === "/src/Temporada.html") {
+
+  // Para conseguir los Capitulos 
+
+
+
+
+  let chapters = [];
+
+  for (let i = 0; i < personajes.length; i++) {
+    // saber si en world ya existe ese nombre del capitulos 
+    let nombrecapitulos = `<div class= "personajesS" > <div class="unoS"> <img class="imageS" src="season1.jpg"> </div> <div class="ChapterNameS"><p> Episode: ${personajes[i].episode[1]}</p></div></div>`
+
+    if (!chapters.includes(nombrecapitulos)) { chapters.push(nombrecapitulos) }
+
+  };
+  let seasonButtons = document.getElementById("seasonZone");
+  seasonButtons.innerHTML = chapters;
+
+  // Otra forma de conseguir episodios 
+  
+  let results = [];
+  const fetchEpisodes = () => {
+    let url = `https://rickandmortyapi.com/api/episode/`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        results['results'] = data.results
+        console.log(results);
+      })
+      fetchEpisodes();
+    let episodioInfo = [];
+    for (let i = 0; i < results.length; i++) {
+      episodioInfo += ` <p class= "nombres" >${results[i].name} </p>`
+      console.log(episodioInfo)
+    }
+    
   };
 
-  //PARA PAGINA DE TEMPORADAS
+  
 
-  if (window.location.pathname === "/src/Temporada.html") {
-
-    // Para conseguir los Capitulos 
-
-    let chapters = [];
-
-    for (let i = 0; i < personajes.length; i++) {
-      // saber si en world ya existe ese nombre del capitulos 
-      let nombrecapitulos = `<div class= "personajesS" <img class= "imageS" src="season1.jpg"> <p> Episode: ${personajes[i].episode[1]}</p></div>`
-
-      if (!chapters.includes(nombrecapitulos)) { chapters.push(nombrecapitulos) }
-
-    };
-
-    let seasonButtons = document.getElementById("seasonZone");
-    seasonButtons.innerHTML = chapters;
-
-    console.log(chapters)
-    // esta } es cierre de if de la ventana
-  };
+  // esta } es cierre de if de la ventana
+};
 
 
-  // Botones paginas 
+// Botones paginas 
 
-  function turnPageC() {
-    window.location.href = 'Personajes.html';
-  }
-  function turnPageW() {
-    window.location.href = 'Mundo.html';
-  }
-  function turnPageS() {
-    window.location.href = 'Temporada.html';
-  }
+function turnPageC() {
+  window.location.href = 'Personajes.html';
+}
+function turnPageW() {
+  window.location.href = 'Mundo.html';
+}
+function turnPageS() {
+  window.location.href = 'Temporada.html';
+}
 
-  document.querySelector('#toCharacters').addEventListener('click', turnPageC)
-  document.querySelector('#toWorlds').addEventListener('click', turnPageW)
-  document.querySelector('#toSeasons').addEventListener('click', turnPageS)
+document.querySelector('#toCharacters').addEventListener('click', turnPageC)
+document.querySelector('#toWorlds').addEventListener('click', turnPageW)
+document.querySelector('#toSeasons').addEventListener('click', turnPageS)
