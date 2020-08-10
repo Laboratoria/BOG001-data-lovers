b8432bc2ff6861e221ba6674b00e7e3aa5a80244
dalejac <k-da929@hotmail.com>
2020-08-10 12:03:01 -0500

    Update OrderBtn

--- src/data.js
+++ src/data.js
@@ -1,20 +1,51 @@
-// estas funciones son de ejemplo
+// ----OTRA FORMA DE ORDENAR----
 
-// export const filter = () => {
-//   Array.prototype.filter()
-
-//   return 'example';
-// };
-
+// export const orderSort = (setDat, sort) => { 
+//   console.log(sort);
+//   if(sort === "A-Z" || sort === "Z-A"){
+//     let sorting = setDat.sort((x,y) => {
+//       if(x.name > y.name ) {
+//         return 1;
+//       } else { return -1;}
+//     }); 
+//     if (sort === "Z-A"){
+//       sorting.reverse();
+//     }
+//       return sorting; 
+//   } else {
+//     let sorting = setDat.sort((x,y) => {
+//       if(x.id > y.id ) {
+//       return 1;
+//     } else { return -1; }
+//   }); 
+//     if (sort === "151-1"){
+//       sorting.reverse();
+//   } 
+//     return sorting; 
+// }}; 
 
 export const orderSort = (setData, order) => {
-  let sorting = setData.sort(function (a,z) {
-    if(a.name > z.name){
-      return 1;
+  let sorting = setData.sort((a,z) => {
+    let result;
+    let x;
+    let y;
+    if(order === "A-Z" || order === "Z-A") {
+      x = a.name;
+      y = z.name;
+    
+    } if(order === "1-151" || order === "151-1") {
+      x = a.id;
+      y = z.id;
     }
-    else { return -1; }
+    if (x > y){ result = 1;}
+    if(x < y) { result = -1;}
+    console.log(result);
+    return result; 
   }) 
-  if (order === "Z-A") {
+  if (order === "Z-A" || order === "151-1") {
     sorting.reverse()
-  } return sorting;
-  };
+  } 
+  return sorting;
+}; 
+
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { orderSort } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
 
-// -----Dar función al botón para cambiar de vista----
+// -----BOTÓN PARA CAMBIAR DE VISTA----
 
 var next = document.getElementById("continue");
 
@@ -11,7 +11,7 @@ next.addEventListener("click", () => {
     document.getElementById("Pokedex").style.display = "block";
 }); 
 
-// ----- Menú y vistas ----
+// ----- MENÚ ----
 // const tabs = Array.prototype.slice.apply(document.querySelectorAll("data-tab-target")); 
 // const window = Array.prototype.slice.apply(document.querySelectorAll("[data-tab-content]")); 
 // document.getElementById("tabs").addEventListener("click", e => { 
@@ -36,21 +36,21 @@ pokedex.addEventListener("click", () => {
     document.getElementById("Pokedex").style.display = "block";
 }); 
 
-// -----Desglosando el objeto de la data----
+// -----DESGLOSANDO EL OBJETO----
 // var info = dataSet[0];
 // var id = info.id;
 // var num = info.num; 
 // var name = info.name;
 // var sum = (`${id} ${num} ${name}`); 
 
-// -----Mostrar los pokemones en el HTML 
+// -----MOSTRAR POKEMONES EN HTML----
 let dataSet = data.pokemon;
 let result;
 for (result of dataSet) {
 document.getElementById("window-container").innerHTML += `<div class="card"><h2> ${result.num} </h2><p> ${result.name} </p><br><img src=${result.img}><div>`; 
 };
 
-// -----Botón de Ordenar----
+// -----BOTÓN DE ORDENAR----
 // let orderBtn = document.getElementById("aZ");
 // orderBtn.addEventListener("change", () => {
 //     var orderOption = orderBtn.options[orderBtn.selectedIndex].text;
@@ -58,18 +58,26 @@ document.getElementById("window-container").innerHTML += `<div class="card"><h2>
 // })
 
 let orderBtn = document.getElementById("aZ");
-orderBtn.addEventListener("change", (event) => {
+orderBtn.addEventListener("change", () => {
     var arrData = orderSort(dataSet, event.target.value); 
-    console.log(arrData);
     var orderOption = document.querySelector(".window-container");
     orderOption.textContent = "";
     for (let pokemon of arrData) {
-        console.log(pokemon);
-    orderOption.innerHTML += `<div class="card"><h2> ${pokemon.num} </h2><p> ${pokemon.name} </p><br><img src=${pokemon.img}><div>`
-} 
-}); 
+    orderOption.innerHTML += `<div class="card"><h2> ${pokemon.num} </h2><p> ${pokemon.name} </p><br><img src=${pokemon.img}><div>`;
+}}); 
 
-// -----Botón de Filtro por Tipos----
+// function numbers () {
+//     var dataArr = sortOrder(dataSet, event.target.value);
+//     var option = document.querySelector(".window-container");
+//     option.textContent = "";
+//     for (let pokemons of dataArr) {
+//         console.log(pokemons);
+//     option.innerHTML += `<div class="card"><h2> ${pokemons.num} </h2><p> ${pokemons.name} </p><br><img src=${pokemons.img}><div>`;
+// }};
+
+// console.log(numbers);
+
+// -----BOTÓN DE TIPOS----
 
 let typeBtn = document.getElementById("element")
 typeBtn.addEventListener("change", (event) => {
@@ -85,12 +93,5 @@ typeBtn.addEventListener("change", (event) => {
 // })
 
 
-// aZ.addEventListener("change", orderBtn);
 
-// function orderBtn () {
-//     var az= event.target.value;
-//     var za = prueba(dataSet, az);
-//       pintarpokemones(za);
-//       console.log(pintarpokemones);
-// }
 
